c432e05711aad841d5a253694d68b4d88d008fc8
Jian Lao <jian.lao@simplisafe.com>
2023-02-09 14:03:39 -0500

    Check point

--- C++_Concurrency/2-SleepAndThreadID.cpp
+++ C++_Concurrency/2-SleepAndThreadID.cpp
@@ -4,6 +4,7 @@
 
 void testFunc()
 {
+    std::cout << "----- testFunc() with ID : " << std::this_thread::get_id() << "\n";
     std::cout << "----- testFunc() : Sleep for 2000ms...\n";
     std::this_thread::sleep_for(std::chrono::milliseconds(2000));
     std::cout << "----- testFunc() : Sleep for 2000ms Done...\n";
@@ -12,21 +13,24 @@ void testFunc()
 
 using namespace std::chrono_literals;
 /* There are 2 ways to generate something like '2000ms':
- * 1. - Use using namespace std::chrono_literals;
+ * 1. - [C++14]
+ *    - Use using namespace std::chrono_literals;
  *    - Use std::this_thread::sleep_for(2000ms);
- * 2. - Use std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+ * 2. - [C++11] 
+ *    - Use std::this_thread::sleep_for(std::chrono::milliseconds(2000));
 */
 
 int main()
 {
-    std::cout << "It is in main() now!\n";
+    std::cout << "It is in main() with ID : " << std::this_thread::get_id() << "\n";
     std::cout << "First create the thread.\n";
     
     std::thread th{testFunc};
+    std::cout << "In main(), created thread ID is : " << th.get_id() << "\n";
     // th.join();
 
     std::cout << "Let main() sleep for 1000ms now...\n";
-    // std::this_thread::sleep_for(2000ms);					// OK
+    // std::this_thread::sleep_for(1000ms);					// OK
     // std::this_thread::sleep_for(std::literals::chrono_literals::1000ms);	// No!
     // std::this_thread::sleep_for(std::chrono_literals::1000ms);		//No!
     
@@ -34,7 +38,8 @@ int main()
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));		// OK
     std::cout << "Main() finished sleeping for 1000ms now...\n";
 
-    th.join();
+    th.join(); // Flow will be blocked at where join() is.
+    std::cout << "In main(), after join(), created thread ID is : " << th.get_id() << "\n";
     return 0;
 
 
