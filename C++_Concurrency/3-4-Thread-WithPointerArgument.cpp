8f8a82cf7eb3a7dfc11f11e3968d410569bdfe2b
Jian Lao <laojian.pku@gmail.com>
2023-08-18 10:44:09 -0400

    Pass by pointer

--- C++_Concurrency/3-4-Thread-WithPointerArgument.cpp
+++ C++_Concurrency/3-4-Thread-WithPointerArgument.cpp
@@ -3,7 +3,12 @@
 
 void printStringPointer(std::string * str)
 {
-    std::cout << "printString : " << str << "\n";
+    // std::cout << "printString : " << str << "\n";
+    /* Above will print like:
+     * printString : 0xffffd03e8f48
+     */
+    
+    std::cout << "printString : " << *str << "\n"; // Print string correctly.
 }
 
 int main(void)
@@ -11,7 +16,7 @@ int main(void)
     std::cout << "In main() - Before creating thread.\n";
     
     std::string str{"Hello Momo"};
-    std::thread testTh(printStringPointer, str);
+    std::thread testTh(printStringPointer, &str);
     testTh.join();
 
     std::cout << "In main() - After thread join().\n";
