3741bbea0f9269e2df405a1b678901a6bdcc6b7e
Jian Lao <laojian.pku@gmail.com>
2023-08-24 00:20:36 -0400

    Check point

--- C++_Concurrency/8-2-Thread-ParentWithExeception.cpp
+++ C++_Concurrency/8-2-Thread-ParentWithExeception.cpp
@@ -8,25 +8,33 @@ void HelloWorld()
 
 int main()
 {
+    std::thread th{HelloWorld};
     try
     {
-	std::thread th{HelloWorld};
-	
-	/* join()_1 */
-	// th.join();
+	// std::thread th{HelloWorld};
 
         throw std::exception();
-        
-	/* join_2 */
+	// When this throw is executed, the stack will be unwinded, so the std::thread obj
+	// will be destructed, before it will be join() ( join()_1 ). So, std::terminate() 
+	// will be called, to terminate the whole program.
+	// 
+	// [Solution-1] : Move std::thread th{HelloWorld} outside try block so throw won't be
+	// able to unwind it (call its destructor); And enable join_2.
+	//
+	// [Solution-2] : RAII.
+	// 
+	// The thread itself, HelloWorld() may, or may not be executed, from test.
+	
+	/* join_1 */
 	th.join();
     }
-    catch(std::exception & e)
+    catch(std::exception & e) // This catch will not be executed.
     {
-        /* join_3 */
-	// th.join();
-        
-	std::cout << "In HelloWorld() - Exception caught : " << e.what() << "\n";
+	std::cout << "In main() - Catch : Exception caught : " << e.what() << "\n";
+        /* join_2 */
+        th.join();
+
     }
 
     return 0;
-}  
+} 
