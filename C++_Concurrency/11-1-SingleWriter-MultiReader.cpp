6d36233edad0da88e3c2aacfc49ea9967a16632c
Jian Lao <laojian.pku@gmail.com>
2023-10-12 22:48:04 -0400

    Multi reader, single writer.

---
+++ C++_Concurrency/11-1-SingleWriter-MultiReader.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <chrono>
+#include <vector>
+
+int x = 0;
+std::mutex rwMutex;
+
+void writer(void)
+{
+    std::lock_guard<std::mutex> wLockGuard(rwMutex);
+    x++;
+    // std::cout << "Writer : x = " << x << std::endl;
+}
+
+void reader(void)
+{
+    std::lock_guard<std::mutex> rLockGuard(rwMutex);
+    // std::cout << "Reader : x = " << x << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+}
+
+int main(void)
+{
+    // Generate 40 threads.
+    std::vector<std::thread> threadVector;
+    
+    for (int i = 0; i < 20; i++)
+    {
+        threadVector.push_back(std::thread(reader));
+    }
+
+    threadVector.push_back(std::thread(writer));
+    threadVector.push_back(std::thread(writer));
+
+    for (int i = 0; i < 20; i++)
+    {
+        threadVector.push_back(std::thread(reader));
+    }
+
+    // Join the generated 40 threads.
+    // Here, don't use "auto th" since auto itself will return a copy while thread can't be copied.
+    // Both "auto &" and "auto &&" are good here.
+    for (auto && th : threadVector)
+    {
+        th.join();
+    }
+
+    return 0;
+}
+
+
