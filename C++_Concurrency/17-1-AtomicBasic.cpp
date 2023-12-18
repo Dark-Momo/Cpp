bfd4d049496ea86c67918ec028b79aec10e86287
Jian Lao <laojian.pku@gmail.com>
2023-12-18 16:29:27 -0500

    Atomic go on

---
+++ C++_Concurrency/17-1-AtomicBasic.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <atomic>
+
+int main(void)
+{
+    std::atomic<int> x = 0;
+    std::cout << "Initialized x = " << x << std::endl;
+    std::cout << "x.load() = " << x.load() << std::endl;
+
+    x.store(1);
+    std::cout << "Initialized x = " << x << std::endl;
+    std::cout << "x.load() = " << x.load() << std::endl;
+
+    std::cout << "x.exchange(2) = " << x.exchange(2) << std::endl;
+    std::cout << "Initialized x = " << x << std::endl;
+    std::cout << "x.load() = " << x.load() << std::endl;
+
+    return 0;
+}
