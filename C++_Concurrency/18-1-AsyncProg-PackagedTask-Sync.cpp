9a921868b8329c3cec3f8c35962a464c485724b8
Jian Lao <laojian.pku@gmail.com>
2024-02-05 22:14:00 -0500

    Packaged_task...

---
+++ C++_Concurrency/18-1-AsyncProg-PackagedTask-Sync.cpp
@@ -0,0 +1,32 @@
+#include <iostream>    // std::cout
+#include <future>      // std::packaged_task, std::future
+#include <chrono>      // std::chrono::seconds
+#include <thread>
+
+int addAfterDelay(int a, int b)
+{
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    return a + b;
+}
+
+int main(void)
+{
+    // We created a task below, but it didn't really run then.
+    std::packaged_task<int(int, int)> pTask(addAfterDelay);
+    
+    // Associate a std::future with it to get its value in current thread.
+    // This is communication for threads, since pTask here is a task.
+    std::future<int> fut = pTask.get_future();
+
+    std::cout << "Before initialize pTask with argument.\n";            // Display immediately.
+
+    // Here we call this task in current thread. 
+    // Under this case, the current calling thread will stop and wait for the new task to finish. 
+    // It is like calling a function, it is Synchoronous;
+    pTask(4, 5);
+    
+    std::cout << "Waitinf for result of addition...\n";                 // Display after 2000ms.
+    std::cout << "Addition of 4 + 5 is : " << fut.get() << std::endl;   // Display after 2000ms.
+
+    return 0;
+}
