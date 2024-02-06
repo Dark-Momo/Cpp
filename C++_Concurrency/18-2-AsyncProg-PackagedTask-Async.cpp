4ef2115ee01734c2e41b9ba48950ff50c4e8b59d
Jian Lao <laojian.pku@gmail.com>
2024-02-05 22:23:12 -0500

    Packaged_task more...

--- C++_Concurrency/18-2-AsyncProg-PackagedTask-Async.cpp
+++ C++_Concurrency/18-2-AsyncProg-PackagedTask-Async.cpp
@@ -14,21 +14,21 @@ int main(void)
     // We created a task below, but it didn't really run then.
     std::packaged_task<int(int, int)> pTask(addAfterDelay);
     
-    // <Knowledge-5-AsynchronousProgram.txt>
     // Associate a std::future with it to get its value in current thread.
     // This is communication for threads, since pTask here is a task.
     std::future<int> fut = pTask.get_future();
 
-    std::cout << "Before initialize pTask with argument.\n";            // Display immediately.
-
     // <Knowledge-5-AsynchronousProgram.txt>
-    // Here we call this task in current thread. 
-    // Under this case, the current calling thread will stop and wait for the new task to finish. 
-    // It is like calling a function, it is Synchoronous;
-    pTask(4, 5);
+    // 1. It is move-only, can't be copied;
+    // 2. In a new thread, passing the task to std::thread's constructor. 
+    //    Under this case, the calling thread and the new thread will run concurrently;
+    // We put this task into a separate thread.
+    std::thread taskTh(std::move(pTask), 4, 5);
     
-    std::cout << "Waitinf for result of addition...\n";                 // Display after 2000ms.
+    std::cout << "Waitinf for result of addition...\n";                 // Display immediately.
     std::cout << "Addition of 4 + 5 is : " << fut.get() << std::endl;   // Display after 2000ms.
+
+    taskTh.join();
 
     return 0;
 }
