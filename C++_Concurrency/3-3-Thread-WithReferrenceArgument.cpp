4682627c28cf2591d8ac253c04bc8084b466cb95
Jian Lao <laojian.pku@gmail.com>
2025-02-17 01:15:52 -0500

    Checkpoint

--- C++_Concurrency/3-3-Thread-WithReferrenceArgument.cpp
+++ C++_Concurrency/3-3-Thread-WithReferrenceArgument.cpp
@@ -4,35 +4,41 @@
 // Pass by reference.
 void printString_1(std::string &str)
 {
-    std::cout << "printString with reference : " << str << "\n";
+    const void* str_addr = static_cast<const void*>((str.c_str()));
+    std::cout << "Address of str in printString_1() is " << str_addr << "\n";
+
+    std::cout << "printString_1() with reference : " << str << "\n";
 }
 
 void printString_2(const std::string &str)
 {
-    std::cout << "printString with const reference : " << str << "\n";
+    std::cout << "printString_2() with [const] reference : " << str << "\n";
 }
 
 void modifyStringRef(std::string &inStr)
 {
-    std::cout << "modifyStringRef : Input str before modifying is : " << inStr << "\n";
+    std::cout << "modifyStringRef() : Input str before modifying is : " << inStr << "\n";
     inStr = "Momo is daddy!";
-    std::cout << "modifyStringRef : Input str after modifying is  : " << inStr << "\n";
+    std::cout << "modifyStringRef() : Input str after modifying is  : " << inStr << "\n";
 }
 
 int main(void)
 {
-    std::cout << "In main() - Before creating thread.\n";
-
     std::string str1{"Hello World"};
+
+    const void* str1_addr = static_cast<const void*>((str1.c_str()));
+    std::cout << "Address of str1 in main() is " << str1_addr << "\n";
+    // Running Result
+    // Address of str1 in main() is 0xffffd73614a8
+    // Address of str in printString_1() is 0xffffd73614a8
+
     std::thread testTh_1(printString_1, std::ref(str1));
     testTh_1.join();
-
     std::cout << "In main() - After thread_1 join().\n";
 
     std::string str2{"Hello Momo"};
     std::thread testTh_2(printString_2, std::cref(str2));
     testTh_2.join();
-
     std::cout << "In main() - After thread_2 join().\n";
 
     std::string str3{"Original String"};
@@ -46,7 +52,7 @@ int main(void)
     // ----------------------------------------------------------------------------------------
     std::thread testTh_3(modifyStringRef, std::ref(str3));
     testTh_3.join();
-
+    std::cout << "In main() - str3 = " << str3 << "\n";
     std::cout << "In main() - After thread_3 join().\n";
 
     return 0;
