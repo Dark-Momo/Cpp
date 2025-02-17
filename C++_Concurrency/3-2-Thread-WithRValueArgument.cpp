0ea07c1e5bffdeadd9c079158024bbeb5be8b256
Jian Lao <laojian.pku@gmail.com>
2025-02-17 01:05:00 -0500

    Checkpoint

--- C++_Concurrency/3-2-Thread-WithRValueArgument.cpp
+++ C++_Concurrency/3-2-Thread-WithRValueArgument.cpp
@@ -4,24 +4,41 @@
 // Pass by RValue.
 void printString(std::string && str)
 {
-    std::cout << "printString : " << str << "\n";
+    const void* str_addr = static_cast<const void*>((str.c_str()));
+    std::cout << "Address of str in printString() is " << str_addr << "\n";
+
+    std::cout << "printString() : " << str << "\n";
 }
 
 int main(void)
-{
-    std::cout << "In main() - Before creating thread.\n";
-    
+{   
     // Pass by RValue.
     std::string str("Hellow World");
+
+    const void* str_addr = static_cast<const void*>((str.c_str()));
+    std::cout << "Address of str in main() is " << str_addr << "\n";
+    
     std::cout << "Before std::move(str), str.empty() = " << str.empty() << "\n";
 
     std::thread testTh(printString, std::move(str));
+    // From below running result:
+    // std::move() will possibly change the object address, since it will cast the object
+    // to be a R-value;
     testTh.join();
 
     std::cout << "After std::move(str), str.empty() = " << str.empty() << "\n";
 
-    std::cout << "In main() - After thread join().\n";
-
     return 0;
 }
+
+// Running Result
+// --------------------------------------------------
+// momo@Ubuntu-Linux-22-04:~/Cpp/C++_Concurrency$ ./a.out 
+// Address of str in main() is 0xffffc91e22d8
+// Before std::move(str), str.empty() = 0
+// Address of str in printString() is 0xaaab144b72d8
+// printString() : Hellow World
+// After std::move(str), str.empty() = 1
+// momo@Ubuntu-Linux-22-04:~/Cpp/C++_Concurrency$ 
+// --------------------------------------------------
 
