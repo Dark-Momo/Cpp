d9c76683eb05b94663ecceaf4d941920e22a018a
Jian Lao <laojian.pku@gmail.com>
2025-02-18 01:12:52 -0500

    Checkpoint

--- C++_Concurrency/4-Native_Handle.cpp
+++ C++_Concurrency/4-Native_Handle.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <thread>
 
+// native_handle() returns the thread handle as a pthread_t on POSIX systems,
+// or as a HANDLE on Windows. 
+// This handle can be used with platform-specific thread management functions. 
+
 void helloWorld(void)
 {
     std::cout << "Hello World!\n";
@@ -8,14 +12,29 @@ void helloWorld(void)
 
 int main(void)
 {
-    std::thread testThread(helloWorld); // Run thread immediately. Funtion name can't be overloaded.
-    std::cout << "Before join(), native_handle() returns " << testThread.native_handle() << "\n";
+    std::thread testThread(helloWorld);
+    
+    // Below shows 3 ways to get thread ID
+    std::cout << "native_handle() returns " << testThread.native_handle() << "\n";
+    
+    // Print the thread ID (POSIX specific)
+    pthread_t handle = testThread.native_handle();
+    std::cout << "Thread ID: " << handle << std::endl;
 
-    // join() will make main() blockingly wait for child thread to finish here.
+    std::cout << "Thread ID: " << testThread.get_id() << std::endl;
+
     testThread.join();
-
     std::cout << "After join(), native_handle() returns " << testThread.native_handle() << "\n";
 
     return 0;
 }
+
+// Running Result
+// momo@Ubuntu-Linux-22-04:~/Cpp/C++_Concurrency$ ./a.out 
+// native_handle() returns 281473724117248
+// Thread ID: 281473724117248
+// Thread ID: 281473724117248
+// Hello World!
+// After join(), native_handle() returns 0
+// momo@Ubuntu-Linux-22-04:~/Cpp/C++_Concurrency$ 
 
--- C++_Concurrency/5-GetID.cpp
+++ C++_Concurrency/5-GetID.cpp
@@ -4,22 +4,23 @@
 
 void HelloWorld()
 {
-    std::cout << "Inside HelloWorld() : thread ID is : " << std::this_thread::get_id() << "\n";
+    std::cout << "Inside HelloWorld() : thread ID : " << std::this_thread::get_id() << "\n";
 }
 
 int main()
 {
-    std::cout << "In main() -  main() has ID : " << std::this_thread::get_id() << "\n";
+    std::cout << "main() has thread ID : " << std::this_thread::get_id() << "\n";
     
     std::thread testThread(HelloWorld);
-    std::cout << "In main() - return thread HelloWorld()'s ID before join(): " << testThread.get_id() << "\n";
+    std::cout << "In main() - testThread's ID before join(): " << testThread.get_id() << "\n";
+    // Here, testThread and function HelloWorld have the same thread ID
 
     testThread.join();
-    std::cout << "In main() - return thread HelloWorld()'s ID after join(): " << testThread.get_id() << "\n";
+    std::cout << "In main() - testThread's ID after join(): " << testThread.get_id() << "\n";
 
     return 0;
 
 }
 
-// Two active thread will have unique ID, while a active thread may have the same ID as a previous active, but already
-// dead thread.
+// Two active thread will have unique ID, while a active thread may have the same ID as a 
+// previous active, but already dead thread.
